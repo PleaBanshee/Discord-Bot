fee9c8933b89521fb06f8a4f65a95003c1996f75
PleaBanshee <llewellynant@gmail.com>
2021-07-30 18:47:47 +0200

    Modified channel statements

--- Main.js
+++ Main.js
@@ -12,11 +12,11 @@ client.on("message", msg => { // bot commands
   let args = msg.content.substring(PREFIX.length).split(' '); // splits entered words with spaces
   switch (args[0]) {
     case 'ping':
-      msg.channel.sendMessage('pong'); // sends message over a channel
+      msg.channel.send('pong'); // sends message over a channel
       break;
   }
   if (msg.content === "hello") {
-    msg.reply( `Hello I'm ${client.user.tag}! Welcome to the server!`);
+    msg.channel.send( `Hello I'm ${client.user.tag}! Welcome to the server!`);
   }
 })
 
