ca6432d3efb188b5dabeefad056d01b5484b5814
PleaBanshee <llewellynant@gmail.com>
2021-07-30 18:44:48 +0200

    Removed unnecessary code

--- Main.js
+++ Main.js
@@ -8,18 +8,15 @@ client.on("ready", () => { // executes when running the script
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on("message", msg => { // message the bot
-  if (msg.content === "hello") {
-    msg.reply( `Hello I'm ${client.user.tag}! Welcome to the server!`);
-  }
-})
-
-client.on("message", msg => { // bot command
+client.on("message", msg => { // bot commands
   let args = msg.content.substring(PREFIX.length).split(' '); // splits entered words with spaces
   switch (args[0]) {
     case 'ping':
       msg.channel.sendMessage('pong'); // sends message over a channel
       break;
+  }
+  if (msg.content === "hello") {
+    msg.reply( `Hello I'm ${client.user.tag}! Welcome to the server!`);
   }
 })
 
